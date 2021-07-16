ec2fa42b857aab0b71b47165c9d702ad4bc10e27
Hamza-18 <hamzabashir18@outlook.com>
2021-07-16 22:10:05 +0500

    completed about page

--- portfolio/static/main.js
+++ portfolio/static/main.js
@@ -1,7 +1,15 @@
- document.addEventListener("DOMContentLoaded",() =>{
-     const navMenu = document.getElementById("nav-menu"),
-         navToggle = document.getElementById("nav-toggle"),
-         navClose = document.getElementById("nav-close");
-     navToggle.onclick = () => navMenu.classList.add("show_menu")
-     navClose.onclick = () =>navMenu.classList.remove("show_menu")
- });
+document.addEventListener("DOMContentLoaded", () => {
+    /* Hide and show menu*/
+    const navMenu = document.getElementById("nav-menu"),
+        navToggle = document.getElementById("nav-toggle"),
+        navClose = document.getElementById("nav-close");
+
+    const showMenu = () => navMenu.classList.add("show_menu")
+    const hideMenu = () => navMenu.classList.remove("show_menu")
+
+    navToggle.onclick = showMenu
+    navClose.onclick = hideMenu
+
+    const navIcon = document.querySelectorAll(".nav_item")
+    navIcon.forEach(icon => icon.onclick = () => hideMenu())
+});
